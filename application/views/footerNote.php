d4eddfb71defe281e6c3bc908af177a98e466e58
G Kosala <kosala4@gmail.com>
2017-12-18 11:22:25 +0530

    Get FooterNOte to single php

---
+++ application/views/footerNote.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by Kosala.
+ * email: kosala4@gmail.com
+ * User: edu
+ * Date: 10/30/17
+ * Time: 2:58 PM
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+        <div class="legal"> 
+            <div class="copyright m-b-10">
+                <div class="align-center">
+                    <a type="button" class="btn btn-block bg-teal waves-effect" href="http://dmb.moe.gov.lk" style="color:white !important;"> Back to Data Management Portal </a>
+                </div>
+            </div>
+            <div class="copyright">
+                &copy;2017 <a href="javascript:void(0);">Data Management Branch</a><br>
+                <a href="javascript:void(0);">Ministry of Education, Sri Lanka</a>
+            </div>
+        </div>
